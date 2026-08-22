2c918c34080a7d429a2427836c4fb90523647b75
Arindam Sharma <117537886+Arindamsharma12@users.noreply.github.com>
2026-08-22 18:26:38 +0530

    swagger ui api docs added

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+import path from "path";
 import routes from "@/routes";
 import { errorHandler } from "@/middlewares/errorHandler";
 
@@ -7,6 +10,10 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
+const swaggerDocument = YAML.load(path.join(process.cwd(), "docs/swagger.yaml"));
+console.log("Swagger loaded:", !!swaggerDocument);
+app.use("/api-docs", swaggerUi.serveFiles(swaggerDocument), swaggerUi.setup(swaggerDocument));
 
 app.get("/health", (_req, res) => {
   res.status(200).json({
